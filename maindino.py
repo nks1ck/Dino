f82d470eb85b043feaf345646e4f1e0d2b5a7c76
nks1ck <nabiullin3553@gmail.com>
2020-09-02 20:35:13 +0300

    Add menu

--- maindino.py
+++ maindino.py
@@ -18,6 +18,7 @@ pygame.mixer.music.set_volume(0.5)
 
 jump_sound = pygame.mixer.Sound('sounds/Jump.wav')
 fall_sound = pygame.mixer.Sound('sounds/Bdish.wav')
+button_sound = pygame.mixer.Sound('sounds/button.wav')
 
 # window icon
 icon = pygame.image.load('img/icon.png')
@@ -98,7 +99,35 @@ class Object:
         display.blit(self.image, (self.x, self.y))
 
 
-def run_game():
+class Button:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.inactive_color = (13, 162, 58)
+        self.active_color = (23, 204, 58)
+
+    def draw(self, x, y, message, action=None, font_size = 30):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
+            pygame.draw.rect(display, self.active_color,
+                             (x, y, self.width, self.height))
+
+            if click[0] == 1:
+                pygame.mixer.Sound.play(button_sound)
+                pygame.time.delay(300)
+                if action is not None:
+                    action()
+
+        else:
+            pygame.draw.rect(display, self.inactive_color,
+                             (x, y, self.width, self.height))
+
+        print_text(message=message, x = x + 10, y = y + 10, font_size=font_size)
+
+
+def game_cycle():
     global jump_dino
     game = True
     cactus_arr = []
@@ -363,7 +392,40 @@ def scores_counter(barriers):
             above_cactus = False
 
 
-while run_game():
+def show_menu():
+    menu_background = pygame.image.load('img/Menu.png')
+    show = True
+    
+    start_button = Button(150, 70)
+    
+
+    while show:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        display.blit(menu_background, (0, 0))
+
+        start_button.draw(330, 200, 'Start game', start_game, 50)
+        pygame.display.update()
+        clock.tick(60)
+
+
+def start_game():
+    global score, make_jump, jump_counter, user_y
+
+    while game_cycle():
+        score = 0
+        make_jump = False
+        jump_counter = 30
+        user_y = display_height - user_height - 100
+
+
+
+show_menu()
+
+while game_cycle():
     score = 0
     make_jump = False
     jump_counter = 30
